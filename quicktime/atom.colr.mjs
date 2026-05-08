691a192051ff561f26c5a59654ca7d2510ee289a
Victor Basumatary <vlenceleuth@gmail.com>
2026-05-08 18:51:38 +0800

    Clean up colr atom documentation

--- quicktime/atom.colr.mjs
+++ quicktime/atom.colr.mjs
@@ -2,17 +2,37 @@ import Atom from './atom.mjs'
 import AtomScanner, { AtomByteReader } from './atom.scanner.mjs'
 
 /**
- * This atom is a required extension for uncompressed Y´CbCr data formats. The 'colr' extension is used to map the numerical values of pixels in the file to a common representation of color in which images can be correctly compared, combined, and displayed. The common representation is the CIE XYZ tristimulus values (defined in Publication CIE No. 15.2).
+ * This atom is a required extension for uncompressed Y´CbCr data
+ * formats. The 'colr' extension is used to map the numerical values of
+ * pixels in the file to a common representation of color in which images
+ * can be correctly compared, combined, and displayed. The common
+ * representation is the CIE XYZ tristimulus values (defined in
+ * Publication CIE No. 15.2).
  *
- * Use of a common representation also allows you to correctly map between Y´CbCr and RGB color spaces and to correctly compensate for gamma on different systems.
+ * Use of a common representation also allows you to correctly map
+ * between Y´CbCr and RGB color spaces and to correctly compensate for
+ * gamma on different systems.
  *
- * The 'colr' extension supersedes the previously defined 'gama' Image Description extension. Writers of QuickTime files should never write both into an Image Description, and readers of QuickTime files should ignore 'gama' if 'colr' is present.
+ * The 'colr' extension supersedes the previously defined 'gama' Image
+ * Description extension. Writers of QuickTime files should never write
+ * both into an Image Description, and readers of QuickTime files should
+ * ignore 'gama' if 'colr' is present.
  *
- * The 'colr' extension is designed to work for multiple imaging applications such as video and print. Each application, driven by its own set of historical and economic realities, has its own set of parameters needed to map from pixel values to CIE XYZ.
+ * The 'colr' extension is designed to work for multiple imaging
+ * applications such as video and print. Each application, driven by its
+ * own set of historical and economic realities, has its own set of
+ * parameters needed to map from pixel values to CIE XYZ.
  *
- * The CIE XYZ representation is mapped to various stored Y´CbCr formats using a common set of transfer functions and matrixes. The transfer function coefficients and matrix values are stored as indexes into a table of canonical references. This provides support for multiple video systems while limiting the scope of possible values to a set of recognized standards.
+ * The CIE XYZ representation is mapped to various stored Y´CbCr formats
+ * using a common set of transfer functions and matrixes. The transfer
+ * function coefficients and matrix values are stored as indexes into a
+ * table of canonical references. This provides support for multiple video
+ * systems while limiting the scope of possible values to a set of
+ * recognized standards.
  *
- * The 'colr' atom contains four fields: a color parameter type and three indexes. The indexes are to a table of primaries, a table of transfer function coefficients, and a table of matrixes.
+ * The 'colr' atom contains four fields: a color parameter type and three
+ * indexes. The indexes are to a table of primaries, a table of transfer
+ * function coefficients, and a table of matrixes.
  *
  * @see {@link https://developer.apple.com/documentation/quicktime-file-format/color_parameter_atom}
  */
